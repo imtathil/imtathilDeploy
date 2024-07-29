9a6b6e3ab7eb6bf87d6237c50a68d2f07792ac3a
DanahHabeeb <82610706+DanahHabeeb@users.noreply.github.com>
2024-07-29 21:40:20 +0300

    vercel

--- web_project/settings.py
+++ web_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o%jdn7fypi)wiqbk3*si)9-=c49vu7lrr_i*@$k^j#h*ivv3(b
 # SECURITY WARNING: don't run with debug turned on in production!!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 
